ef7575ed10b829ef484c1c378a00709d7bda1d52
genkidama420 <a01504276@unet.univie.ac.at>
2021-06-15 21:41:40 +0200

    coreset seems to be done

--- Coreset.py
+++ Coreset.py
@@ -11,23 +11,19 @@ class Coreset:
     def get_q(self):
 
         # calculates the q for every sample in the dataset
-
         self.q = np.zeros(self.X.shape[0])
         q_sum = np.sum(np.linalg.norm(self.X - self.mu)**2)
- 
-        X_abs = np.linalg.norm(self.X)**2
 
         for i, x in tqdm(enumerate(self.X)):
             # calculates the q(x)
-
-            q = (1/X_abs) + ((np.linalg.norm(x - self.mu)**2 / q_sum))
+            q = 0.5 * (1/self.X.shape[0]) + 0.5 * (np.linalg.norm(x - self.mu)**2 / q_sum)
 
             self.q[i] = q
         
 
     def get_coreset(self, m):
         # chooses m random samples 
-        rnd_indices = np.random.choice(self.X.shape[0], m, replace=True, p=self.q)
+        rnd_indices = np.random.choice(self.X.shape[0], m, p=self.q)
 
         # calculates the m weighted, randomly selected points
         C = np.zeros((m, self.X.shape[1]))
@@ -35,4 +31,4 @@ class Coreset:
         for i, idx in enumerate(rnd_indices):
             C[i] = 1 / (m * self.q[idx]) * self.X[idx]
 
-        return C
+        return C, rnd_indices

5d35b80b86b2729baddc16b1144723a0a91bc745
genkidama420 <a01504276@unet.univie.ac.at>
2021-06-15 17:10:57 +0200

    replaced distance function with scipy cdist

--- Coreset.py
+++ Coreset.py
@@ -1,14 +1,16 @@
 import numpy as np
+from scipy.spatial.distance import cdist
 from tqdm import tqdm
 
 class Coreset:
     
     def __init__(self, X):
         self.X = X
-        self.mu = np.mean(X, axis=0)
+        self.mu = np.mean(X, axis=0, keepdims=True)
 
     def dist(self, x, y):
         # Took this formulation from the sklearn euclidean distance documentation
+        # Deprecated, replaced by scipy cdist with sqeuclidean
         return np.sqrt(np.dot(x, y) - 2 * np.dot(x,y) + np.dot(y, y))**2
     
     def get_q(self):
@@ -16,22 +18,23 @@ class Coreset:
         # calculates the q for every sample in the dataset
 
         self.q = []
-        q_sum = np.sum(self.dist(self.X, self.mu))  # produces a sum of NaN, needs fixing
-                                                    # possibly in dist function
+        q_sum = np.sum(cdist(self.X, self.mu, metric='sqeuclidean'), keepdims=True)  
+        print(q_sum)
+
         X_abs = np.abs(self.X)
 
 
         for x in tqdm(self.X):
             # calculates the q(x)
             self.q.append( 1.0 / (2.0 * X_abs) + 1.0 / 2.0 * \
-                            (self.dist(x, self.mu) / q_sum) )
+                            (cdist(x.reshape((1,74)), self.mu, metric='sqeuclidean') / q_sum) )
         
         return self.q
 
     def get_coreset(self, m):
 
         # chooses m random samples 
-        idx = np.random.choice(self.X.shape[0], m, replace=False)
+        idx = np.random.choice(self.X.shape[0], m, replace=True, p=self.q)
 
         # calculates the m weighted, randomly selected points
         C = 1 / (m * self.q[idx]) * self.X[idx]
@@ -40,7 +43,6 @@ class Coreset:
 
 
 data = np.genfromtxt("data/bio_train.csv", delimiter=",")[:,3:]
-print(data.shape)
 labels_true = np.genfromtxt("data/bio_train.csv", delimiter=",")[:,0]
 
 coreset = Coreset(data)
